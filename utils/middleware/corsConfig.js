284eb5d665970720fae859662b6692265d1003be
Leon Omollo <leonofaugust@gmail.com>
2022-11-09 13:37:15 +0100

    the product controller now gets the seller id used in creating a product from the accesstoken decoded from the post request, the corsConfig file now has no console logs

--- utils/middleware/corsConfig.js
+++ utils/middleware/corsConfig.js
@@ -20,7 +20,6 @@ const corsOptions = {
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
-    console.log(origin)
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
     }
